9c990a247d7506a13286062eb7fe796230aaa077
assuncaovictor <victorassuncao3382@gmail.com>
2023-05-24 18:14:00 -0300

    <Feat>: Alterando o exemplo para o do professor

--- src/domain/Home/state/hooks/useSaldo.js
+++
@@ -1,9 +0,0 @@
-import { useRecoilValue, useSetRecoilState } from "recoil";
-import { saldo as atomSaldo } from "../atom";
-
-export const useSaldo = () => {
-    const saldo = useRecoilValue(atomSaldo);
-    const setSaldo = useSetRecoilState(atomSaldo);
-
-    return { saldo, setSaldo };
-};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./config/routes";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
